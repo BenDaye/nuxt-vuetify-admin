a1aab0b91063d8bdfb56904d597aeaf1bb3bb853
BenDaye <yypsgdsg@163.com>
2020-10-28 00:10:08 +0800

    fix: unnecessary middleware

--- middleware/id.js
+++
@@ -1,8 +0,0 @@
-export default ({ app, params, $dialog }) => {
-  if (!params.id) {
-    if (process.client) {
-      $dialog.message.warning('缺少必要参数', { position: 'top-right' })
-    }
-    return app.$router.back()
-  }
-}
